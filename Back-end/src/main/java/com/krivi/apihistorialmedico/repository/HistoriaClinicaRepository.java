faccfcb520786b14d439ccc597d92109de4c7f3b
Rafael <52611808+Hypnos123@users.noreply.github.com>
2026-06-22 23:27:10 -0500

    Corrige encabezado al visualizar consulta desde historia clínica

--- Back-end/src/main/java/com/krivi/apihistorialmedico/repository/HistoriaClinicaRepository.java
+++ Back-end/src/main/java/com/krivi/apihistorialmedico/repository/HistoriaClinicaRepository.java
@@ -3,9 +3,11 @@ package com.krivi.apihistorialmedico.repository;
 import com.krivi.apihistorialmedico.model.entity.HistoriaClinica;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface HistoriaClinicaRepository extends CrudRepository<HistoriaClinica, Integer> {
   Optional<HistoriaClinica> findByPacienteIdPaciente(Integer idPaciente);
   boolean existsByPacienteIdPaciente(Integer idPaciente);
+  long countByFechaCreacionGreaterThanEqualAndFechaCreacionLessThan(LocalDateTime inicio, LocalDateTime fin);
 }
