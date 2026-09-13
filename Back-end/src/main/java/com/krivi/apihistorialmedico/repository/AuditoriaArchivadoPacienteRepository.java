e1ab3be888b065853f8018cfb3d69d60b2fe15b3
Hypnos123 <josset_0209@hotmail.com>
2026-09-13 17:03:19 -0500

    Corregir import Pageable en repositorio de auditoria

--- Back-end/src/main/java/com/krivi/apihistorialmedico/repository/AuditoriaArchivadoPacienteRepository.java
+++ Back-end/src/main/java/com/krivi/apihistorialmedico/repository/AuditoriaArchivadoPacienteRepository.java
@@ -4,6 +4,8 @@ import com.krivi.apihistorialmedico.model.entity.AuditoriaArchivadoPaciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Pageable;
+
 
 import java.time.LocalDateTime;
 import java.util.List;
